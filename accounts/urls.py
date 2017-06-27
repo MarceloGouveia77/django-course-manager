29822e64e90ef271fe308ce36d76bd4383429d75
Marcelo Gouveia <tson666@gmail.com>
2017-06-27 02:25:03 -0300

    edit password & account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib.auth.views import login as authLogin
 from django.contrib.auth.views import logout as authLogout
-from .views import register
+from .views import register, dashboard, edit, edit_password
 
 urlpatterns = [
+    url(r'^$', dashboard, name='dashboard'),
+    url(r'^editar/$', edit, name='edit'),
+    url(r'^editar-senha/$', edit_password, name='edit_password'),
     url(r'^entrar/$', authLogin,
+
     {'template_name': 'accounts/login.html'},
     name='login'),
 
